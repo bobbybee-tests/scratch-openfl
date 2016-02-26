2498d9a9520302bf8a3c0c53419493347e5fc89c
Alyssa <alyssa@backtick.town>
2016-02-25 19:05:38 -0500

    ..

--- src/svgeditor/Selection.hx
+++ src/svgeditor/Selection.hx
@@ -414,7 +414,7 @@ class Selection implements IEventDispatcher
                 refDispObj.transform.matrix = m;
         }
 
-        public function getGlobalBoundingPoints()Dynamic {
+        public function getGlobalBoundingPoints() : Dynamic {
                 var r:Rectangle = getBounds(refDispObj);
 
                 return {
